7038220cbad968d35e1f3d7a81ea34bcd4b67e30
Muhammad Umer <mhadisb1212@gmail.com>
2024-05-05 12:25:25 +0500

    added controllers to auth_field

--- lib/features/auth/presentation/pages/signup_page.dart
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -11,41 +11,64 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Sign Up',
-              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 30),
-            const AuthField(hintText: 'Name'),
-            const SizedBox(height: 10),
-            const AuthField(hintText: 'Email'),
-            const SizedBox(height: 10),
-            const AuthField(hintText: 'Password'),
-            const SizedBox(height: 20),
-            const AuthGradientButton(text: "Sign Up"),
-            const SizedBox(height: 10),
-            RichText(
-              text: TextSpan(
-                  text: "Already have an account? ",
-                  style: Theme.of(context).textTheme.titleMedium,
-                  children: [
-                    TextSpan(
-                      text: "Sign In",
-                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                          color: AppPallete.gradient2,
-                          fontWeight: FontWeight.w600),
-                    ),
-                  ]),
-            ),
-          ],
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Sign Up',
+                style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 30),
+              AuthField(hintText: 'Name', controller: nameController),
+              const SizedBox(height: 10),
+              AuthField(hintText: 'Email', controller: emailController),
+              const SizedBox(height: 10),
+              AuthField(
+                hintText: 'Password',
+                controller: passwordController,
+                isObscureText: true,
+              ),
+              const SizedBox(height: 20),
+              const AuthGradientButton(text: "Sign Up"),
+              const SizedBox(height: 10),
+              RichText(
+                text: TextSpan(
+                    text: "Already have an account? ",
+                    style: Theme.of(context).textTheme.titleMedium,
+                    children: [
+                      TextSpan(
+                        text: "Sign In",
+                        style: Theme.of(context)
+                            .textTheme
+                            .titleMedium
+                            ?.copyWith(
+                                color: AppPallete.gradient2,
+                                fontWeight: FontWeight.w600),
+                      ),
+                    ]),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/features/auth/presentation/widgets/auth_field.dart
+++ lib/features/auth/presentation/widgets/auth_field.dart
@@ -2,15 +2,24 @@ import 'package:flutter/material.dart';
 
 class AuthField extends StatelessWidget {
   final String hintText;
+  final TextEditingController controller;
+  final bool isObscureText;
 
-  const AuthField({super.key, required this.hintText});
+  const AuthField(
+      {super.key,
+      required this.hintText,
+      required this.controller,
+      this.isObscureText = false});
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      controller: controller,
       decoration: InputDecoration(
         hintText: hintText,
       ),
+      validator: (value) => value!.isEmpty ? '$hintText is missing!' : null,
+      obscureText: isObscureText,
     );
   }
 }
